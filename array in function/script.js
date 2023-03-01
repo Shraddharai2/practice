a41f39bd5d1de575e7e467c89d8755d45ead6bb2
Shraddha Rai <119661524+Shraddharai2@users.noreply.github.com>
2023-03-01 15:51:00 +0530

    Add files via upload

--- array in function/script.js
+++ array in function/script.js
@@ -10,3 +10,19 @@ function sum(arr){
  let sumarray = [1,2,3,4,5,6,7,8];
  let arrresult = sum(sumarray);
  console.log(arrresult);
+
+
+
+
+//  when we don't pass and arguments then -
+
+function me(){
+    let sum = 0;
+    // when we don't pass any arguments then system by default take it as arguments
+    for(let i = 0; i< arguments.length; i ++){
+        sum = sum + arguments[i];
+    } 
+    return sum;
+}
+
+console.log (me(7,8,9,10,11,12,13,14,15));
