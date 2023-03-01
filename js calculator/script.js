4ac6c9eca167500240a3998e6ab7f047fbcbd5d9
Shraddha Rai <119661524+Shraddharai2@users.noreply.github.com>
2023-03-01 14:30:54 +0530

    Add files via upload

---
+++ js calculator/script.js
@@ -0,0 +1,41 @@
+// here i am trying to make calculator with the help of fuction
+
+function sum(val1,val2){
+    let result= val1 + val2;
+    return result;
+}
+
+let add = sum(40,40);
+console.log(add);
+
+function sub(val1,val2){
+    let result = val1 - val2;
+    return result;
+}
+
+let minus = sub(80,20);
+console.log(minus);
+
+function div(val1,val2){
+    let result = val1 / val2;
+    return result;
+}
+
+let divi = div(400,20);
+console.log(divi);
+
+function mul(val1,val2){
+    let result = val1 * val2;
+    return result;
+}
+
+let multi = mul(20,8);
+console.log(multi);
+
+function percen(val1,val2){
+    let result = val1 % val2;
+    return result;
+}
+
+let per = percen(9,2);
+console.log(per);
